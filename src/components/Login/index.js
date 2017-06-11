7e65f5bbbe045ddaba0e0c1c9636944717da84e3
Rob Calcroft <rob@calcroft.com>
2017-06-11 23:33:56 +0100

    First commit

---
+++ src/components/Login/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Login.scss';
+
+export default () => (
+  <h1 className={styles.container}>Rob</h1>
+);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/body.scss';
+
+// Components
+import Login from './components/Login';
+
+ReactDOM.render(<Login />, document.getElementById('opsview-web'));
